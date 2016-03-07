d4e9a086900b1241279e363f06d83e538e682e8e
zhitongLBN <z.liu@linkbynet.com>
2016-03-07 16:40:09 +0100

    adapte rubocop style

--- lib/fog/openstack/requests/compute/create_volume.rb
+++ lib/fog/openstack/requests/compute/create_volume.rb
@@ -37,21 +37,19 @@ module Fog
         def create_volume(name, description, size, options = {})
           response = Excon::Response.new
           response.status = 202
-          data = {
-            'id'                  => Fog::Mock.random_numbers(2),
-            'displayName'         => name,
-            'displayDescription'  => description,
-            'size'                => size,
-            'status'              => 'creating',
-            'snapshotId'          => options[:snapshot_id],
-            'volumeType'          => options[:volume_type] || 'None',
-            'availabilityZone'    => options[:availability_zone] || 'nova',
-            'createdAt'           => Time.now.strftime('%FT%T.%6N'),
-            'attachments'         => [],
-            'metadata'            => options[:metadata] || {}
+          data = {'id' => Fog::Mock.random_numbers(2),
+                  'displayName' => name,
+                  'displayDescription' => description,
+                  'size' => size,
+                  'status' => 'creating',
+                  'snapshotId' => options[:snapshot_id],
+                  'volumeType' => options[:volume_type] || 'None',
+                  'availabilityZone' => options[:availability_zone] || 'nova',
+                  'createdAt' => Time.now.strftime('%FT%T.%6N'),
+                  'attachments' => [], 'metadata' => options[:metadata] || {}
           }
           self.data[:volumes][data['id']] = data
-          response.body = { 'volume' => data }
+          response.body = {'volume' => data}
           response
         end
       end
