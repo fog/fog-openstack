5a2c07110c1071df8e4e6b1b93782dcd413c0590
Darren Hague <d.hague@sap.com>
2016-04-29 10:01:29 +0100

    Volume API - add missing "volume update" methods

--- lib/fog/openstack/models/volume/volume.rb
+++ lib/fog/openstack/models/volume/volume.rb
@@ -30,6 +30,36 @@ module Fog
         def ready?
           status == 'available'
         end
+
+        def reset_status(status)
+          requires :id
+          service.action(id, {'os-reset_status' => {:status => status}})
+        end
+
+        def create_metadata(metadata)
+          replace_metadata(metadata)
+        end
+
+        # Existing keys have values updated and new key-value pairs are created, but none are deleted
+        def update_metadata(metadata)
+          requires :id
+          service.update_metadata(id, metadata)
+          true
+        end
+
+        # All existing key-value pairs are deleted and replaced with the key-value pairs specified here
+        def replace_metadata(metadata)
+          requires :id
+          service.replace_metadata(id, metadata)
+          true
+        end
+
+        # Delete one specific key-value pair by specifying the key name
+        def delete_metadata(key_name)
+          requires :id
+          service.delete_metadata(id, key_name)
+          true
+        end
       end
     end
   end
