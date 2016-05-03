5e69dd1711c5bdb98503e0fd23b11f6cb341de76
Darren Hague <d.hague@sap.com>
2016-05-03 16:15:33 +0100

    Add missing VCR recordings & fix Hound-reported style errors

--- lib/fog/openstack/requests/volume_v2/create_snapshot.rb
+++ lib/fog/openstack/requests/volume_v2/create_snapshot.rb
@@ -7,14 +7,14 @@ module Fog
         class Real
           include Fog::Volume::OpenStack::Real
 
-          def create_snapshot(volume_id, name, description, force=false)
+          def create_snapshot(volume_id, name, description, force = false)
             data = {
-                'snapshot' => {
-                    'volume_id'   => volume_id,
-                    'name'        => name,
-                    'description' => description,
-                    'force'       => force.nil? ? false : force
-                }
+              'snapshot' => {
+                'volume_id'   => volume_id,
+                'name'        => name,
+                'description' => description,
+                'force'       => force.nil? ? false : force
+              }
             }
 
             _create_snapshot(data)
@@ -22,19 +22,19 @@ module Fog
         end
 
         class Mock
-          def create_snapshot(volume_id, name, description, force=false)
+          def create_snapshot(volume_id, name, description, _force = false)
             response        = Excon::Response.new
             response.status = 202
             response.body   = {
-                "snapshot" => {
-                    "status"      => "creating",
-                    "name"        => name,
-                    "created_at"  => Time.now,
-                    "description" => description,
-                    "volume_id"   => volume_id,
-                    "id"          => "5",
-                    "size"        => 1
-                }
+              "snapshot" => {
+                "status"      => "creating",
+                "name"        => name,
+                "created_at"  => Time.now,
+                "description" => description,
+                "volume_id"   => volume_id,
+                "id"          => "5",
+                "size"        => 1
+              }
             }
             response
           end
