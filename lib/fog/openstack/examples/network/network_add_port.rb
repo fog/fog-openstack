0b64edf20306adebaa6f354296683a9323de7c67
rb1whitney <rb1whitney@msn.com>
2016-05-05 21:56:48 -0600

    Fixing broken align

--- lib/fog/openstack/examples/network/network_add_port.rb
+++ lib/fog/openstack/examples/network/network_add_port.rb
@@ -3,11 +3,11 @@ require 'fog/openstack'
 # Add additional port to an Openstack node
 
 def create_virtual_address_pairing(username, password, auth_url, tenant, device_id, device_ip_address, network_id)
-  network_driver = Fog::Network.new(:provider => :openstack,
-                                    :openstack_api_key => password,
+  network_driver = Fog::Network.new(:provider           => :openstack,
+                                    :openstack_api_key  => password,
                                     :openstack_username => username,
                                     :openstack_auth_url => auth_url,
-                                    :openstack_tenant => tenant)
+                                    :openstack_tenant   => tenant)
 
   virtual_ip_address = network_driver.create_port(network_id)
 
