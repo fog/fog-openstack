1b8155c29261d8ad40bea5a97874ed48d7066764
rb1whitney <rb1whitney@msn.com>
2016-05-05 21:41:33 -0600

    Adding Example and main fix

--- lib/fog/openstack/requests/network/update_port.rb
+++ lib/fog/openstack/requests/network/update_port.rb
@@ -6,7 +6,7 @@ module Fog
           data = { 'port' => {} }
 
           vanilla_options = [:name, :fixed_ips, :admin_state_up, :device_owner,
-                             :device_id, :security_groups]
+                             :device_id, :security_groups, :allowed_address_pairs]
           vanilla_options.select{ |o| options.key?(o) }.each do |key|
             data['port'][key] = options[key]
           end
@@ -24,13 +24,13 @@ module Fog
         def update_port(port_id, options = {})
           response = Excon::Response.new
           if port = list_ports.body['ports'].find { |_| _['id'] == port_id }
-            port['name']              = options[:name]
-            port['fixed_ips']         = options[:fixed_ips] || []
-            port['admin_state_up']    = options[:admin_state_up]
-            port['device_owner']      = options[:device_owner]
-            port['device_id']         = options[:device_id]
-            port['security_groups']   = options[:security_groups] || []
-            port['allowed_address_pairs']   = options[:allowed_address_pairs] || []
+            port['name']                  = options[:name]
+            port['fixed_ips']             = options[:fixed_ips] || []
+            port['admin_state_up']        = options[:admin_state_up]
+            port['device_owner']          = options[:device_owner]
+            port['device_id']             = options[:device_id]
+            port['security_groups']       = options[:security_groups] || []
+            port['allowed_address_pairs'] = options[:allowed_address_pairs] || []
             response.body = { 'port' => port }
             response.status = 200
             response
