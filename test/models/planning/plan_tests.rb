4c6109b03860131fa37ee10d1da3cc580f5dfc53
Gilles Dubreuil <gilles@redhat.com>
2016-05-25 06:48:45 +1000

    Shindo to Minitest: identity, image, planning
    Migrates identity, image and planning from Shindo to Minitest
    Note: This is not using subfolder 'openstack' anymore

---
+++ test/models/planning/plan_tests.rb
@@ -0,0 +1,63 @@
+require "test_helper"
+
+describe "Fog::Openstack[:planning] | plan" do
+  describe "success" do
+    before do
+      @planning = Fog::Openstack[:planning]
+      @instance = @planning.plans.first
+      @role = @planning.list_roles.body.first
+    end
+
+    it "#add_role" do
+      @instance.add_role(@role['uuid']).body["roles"][0]["uuid"].must_equal @role['uuid']
+    end
+
+    it "#templates" do
+      @instance.templates.wont_be_empty
+    end
+
+    it "#master_template" do
+      @instance.master_template.must_be_kind_of String
+    end
+
+    it "#environment" do
+      @instance.environment.must_be_kind_of String
+    end
+
+    it "#provider_resource_templates" do
+      @instance.provider_resource_templates["provider-compute-1.yaml"].wont_be_empty
+    end
+
+    it "#patch" do
+      parameter = @instance.parameters.first
+      @instance.patch(
+        :parameters => [
+          {
+            "name"  => parameter['name'],
+            "value" => 'new_value'
+          }
+        ]
+      )["uuid"].must_be_kind_of String
+    end
+
+    it "#remove_role" do
+      @instance.remove_role(@role['uuid']).status.must_equal 200
+    end
+
+    it "#save" do
+      @instance.save.must_be_kind_of Fog::Openstack::Planning::Plan
+    end
+
+    it "#update" do
+      @instance.update.uuid.wont_be_empty
+    end
+
+    it "#destroy" do
+      @instance.destroy.must_equal true
+    end
+
+    it "#create" do
+      @instance.create.must_be_kind_of Fog::Openstack::Planning::Plan
+    end
+  end
+end
---
+++ test/requests/identity/user_tests.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
+
+describe "Fog::Identity[:openstack] | user requests" do
+  before do
+    @identity = Fog::Identity[:openstack]
+
+    @user_format = {
+      'id'       => String,
+      'name'     => String,
+      'enabled'  => Fog::Boolean,
+      'email'    => String,
+      'tenantId' => Fog::Nullable::String
+    }
+
+    @user_name = Fog::Mock.random_hex(64)
+    @user_name_update = Fog::Mock.random_hex(64)
+
+    @user = @identity.create_user(
+      @user_name, "mypassword", "morph@example.com",
+      OpenStack::Identity.get_tenant_id(@identity)
+    ).body['user']
+  end
+
+  describe "success" do
+    it "#create_user(#{@user_name}, 'mypassword', 'morph@example.com', 't3n4nt1d', true)" do
+      @user.must_match_schema(@user_format, nil, :allow_optional_rules => false)
+    end
+
+    it "#list_users" do
+      @identity.list_users.body["users"][0].must_match_schema(@user_format)
+    end
+
+    it "#get_user_by_id" do
+      @identity.get_user_by_id(@user['id']).body['user'].must_match_schema(@user_format)
+    end
+
+    it "#get_user_by_name" do
+      @identity.get_user_by_name(@user['name']).body['user'].must_match_schema(@user_format)
+    end
+
+    it "#update_user" do
+      @identity.update_user(
+        @user['id'], :name => @user_name_update, :email => 'fog@test.com'
+      ).status.must_equal 200
+    end
+
+    it "#delete_user" do
+      @identity.delete_user(@user['id']).status.must_equal 204
+    end
+  end
+end
