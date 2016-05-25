eb64cc00101f59f1b76aaa5dab86eb7eb66496f5
Gilles Dubreuil <gilles@redhat.com>
2016-05-25 22:14:32 +1000

    Shindo to Minitest: common4
    Adds a migrated version of collection_helper.rb & model_helper.rb
    helpers to Minitest in /test/helpers
    
    Also, adds a UNIT_TESTS_CLEAN flag to skip broken test

---
+++ test/helpers/model_helper.rb
@@ -0,0 +1,31 @@
+def model_tests(collection, params = {})
+  describe "success" do
+    before do
+      @instance = collection.new(params)
+
+      if block_given?
+        yield(@instance)
+      end
+    end
+
+    it "#save" do
+      skip if Fog.mocking?
+      @instance.save.must_equal true
+    end
+
+    it "#destroy" do
+      skip if Fog.mocking?
+      @instance.destroy.must_equal 200
+    end
+  end
+end
+
+# Generates a unique identifier with a random differentiator.
+# Useful when rapidly re-running tests, so we don't have to wait
+# serveral minutes for deleted objects to disappear from the API
+# E.g. 'fog-test-1234'
+def uniq_id(base_name = 'fog-test')
+  # random_differentiator
+  suffix = rand(65536).to_s(16).rjust(4, '0')
+  [base_name, suffix].join '-'
+end
