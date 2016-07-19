8ea8a9d2ba9d5308240f7f189c8d93a88f438e83
He Qi <foreverqihe@gmail.com>
2016-07-20 09:31:24 +1000

    template or template_url should not both exist as the Heat API takes template over template_url even if template is null

--- lib/fog/openstack/models/orchestration/stack.rb
+++ lib/fog/openstack/models/orchestration/stack.rb
@@ -98,18 +98,9 @@ module Fog
 
         # build options to create or update stack
         def default_options
-          template_content =
-            if template && template.is_a?(Fog::Orchestration::OpenStack::Template)
-              template.content
-            else
-              template
-            end
-
           {
             :stack_name       => stack_name,
             :disable_rollback => disable_rollback,
-            :template_url     => @template_url,
-            :template         => template_content,
             :timeout_mins     => timeout_mins
           }
         end
