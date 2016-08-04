ce61656375c84349bb95e5c85f48e647efbd95f4
Torsten Lesmann <torsten.lesmann@sap.com>
2016-08-04 16:05:43 +0200

    Load Balancer: lbaasv2 first draft

---
+++ lib/fog/network/openstack/requests/create_lbaas_loadbalancer.rb
@@ -0,0 +1,54 @@
+module Fog
+  module Network
+    class OpenStack
+      class Real
+        def create_lbaas_loadbalancer(vip_subnet_id, options = {})
+          data = {
+              'loadbalancer' => {
+                  'vip_subnet_id' => vip_subnet_id
+              }
+          }
+
+          vanilla_options = [:name, :description, :vip_address, :provider, :flavor, :admin_state_up, :tenant_id]
+          vanilla_options.reject { |o| options[o].nil? }.each do |key|
+            data['loadbalancer'][key] = options[key]
+          end
+
+          request(
+              :body => Fog::JSON.encode(data),
+              :expects => [201],
+              :method => 'POST',
+              :path => 'lbaas/loadbalancers'
+          )
+        end
+      end
+
+      class Mock
+        def create_lbaas_loadbalancer(subnet_id, pool_id, protocol, protocol_port, options = {})
+          response = Excon::Response.new
+          response.status = 201
+          data = {
+              'id' => Fog::Mock.random_numbers(6).to_s,
+              'subnet_id' => subnet_id,
+              'pool_id' => pool_id,
+              'protocol' => protocol,
+              'protocol_port' => protocol_port,
+              'name' => options[:name],
+              'description' => options[:description],
+              'address' => options[:address],
+              'port_id' => Fog::Mock.random_numbers(6).to_s,
+              'session_persistence' => options[:session_persistence],
+              'connection_limit' => options[:connection_limit],
+              'status' => 'ACTIVE',
+              'admin_state_up' => options[:admin_state_up],
+              'tenant_id' => options[:tenant_id],
+          }
+
+          self.data[:lb_vips][data['id']] = data
+          response.body = {'vip' => data}
+          response
+        end
+      end
+    end
+  end
+end
