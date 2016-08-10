faa1a34672b22e498cecf5cef7d2188312760b37
Tzu-Mainn Chen <tzumainn@redhat.com>
2016-08-10 16:24:32 +0200

    add support for cinder backups

--- lib/fog/volume/openstack/v2.rb
+++ lib/fog/volume/openstack/v2.rb
@@ -28,6 +28,9 @@ module Fog
         model :transfer
         collection :transfers
 
+        model :backup
+        collection :backups
+
         request_path 'fog/volume/openstack/v2/requests'
 
         # Volume
@@ -62,6 +65,13 @@ module Fog
         request :get_transfer_details
         request :accept_transfer
         request :delete_transfer
+
+        request :list_backups
+        request :list_backups_detailed
+        request :create_backup
+        request :get_backup_details
+        request :restore_backup
+        request :delete_backup
 
         request :update_quota
         request :get_quota
