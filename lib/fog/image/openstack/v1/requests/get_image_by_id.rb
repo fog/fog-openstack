4dc59c149d6ad7b3765f17d8d00a5633c562f194
Sean Handley <sean@DixieBook.local>
2016-12-19 14:45:08 +0000

    Don't rely on #all because responses are paged at 1000 entries.

--- lib/fog/image/openstack/v1/requests/get_image_by_id.rb
+++ lib/fog/image/openstack/v1/requests/get_image_by_id.rb
@@ -5,27 +5,35 @@ module Fog
         class Real
           def get_image_by_id(image_id)
             request(
-              :expects => [200, 204],
-              :method  => 'GET',
+              :expects => [200],
+              :method  => 'HEAD',
               :path    => "images/#{image_id}"
             )
           end
         end # class Real
 
         class Mock
-          def get_image_by_id(_image_id)
+          def get_image_by_id(image_id)
             response = Excon::Response.new
             response.status = [200, 204][rand(2)]
-            response.body = {
-              "images" => [{
-                "name"             => "mock-image-name",
-                "size"             => 25165824,
-                "disk_format"      => "ami",
-                "container_format" => "ami",
-                "id"               => "0e09fbd6-43c5-448a-83e9-0d3d05f9747e",
-                "checksum"         => "2f81976cae15c16ef0010c51e3a6c163"
-              }]
+            response.headers = {
+              'X-Image-Meta-Checksum'         => '8a40c862b5735975d82605c1dd395796',
+              'X-Image-Meta-Container_format' => 'aki',
+              'X-Image-Meta-Created_at'       => '2016-01-06T03:22:20.000000',
+              'X-Image-Meta-Deleted'          => 'False',
+              'X-Image-Meta-Disk_format'      => 'aki',
+              'X-Image-Meta-Id'               => image_id,
+              'X-Image-Meta-Is_public'        => 'True',
+              'X-Image-Meta-Min_disk'         => 0,
+              'X-Image-Meta-Min_ram'          => 0,
+              'X-Image-Meta-Name'             => 'cirros-0.3.4-x86_64-uec-kernel',
+              'X-Image-Meta-Owner'            => '13cc6052265b41529e2fd0fc461fa8ef',
+              'X-Image-Meta-Protected'        => 'False',
+              'X-Image-Meta-Size'             => 4979632,
+              'X-Image-Meta-Status'           => 'deactivated',
+              'X-Image-Meta-Updated_at'       => '2016-02-25T03:02:05.000000'
             }
+            response.body = {}
             response
           end # def list_tenants
         end # class Mock
--- spec/image_v1_spec.rb
+++ spec/image_v1_spec.rb
@@ -19,13 +19,13 @@ describe Fog::Image::OpenStack do
   describe 'find_by_id' do
     it 'finds image' do
       existing_image_id = 'ea20c966-d2fb-4287-a2eb-7bece9af4263'
-      VCR.use_cassette('list_images') do
+      VCR.use_cassette('images_v1_find_by_id') do
         @service.images.find_by_id(existing_image_id).id.must_equal existing_image_id
       end
     end
 
     it 'returns nil when image is not found' do
-      VCR.use_cassette('list_images') do
+      VCR.use_cassette('images_v1_find_by_id') do
         assert_nil @service.images.find_by_id('non-existing-id')
       end
     end
