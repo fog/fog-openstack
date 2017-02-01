289a49f64eef2a1e96daadf7395bbcd840b081a4
Maurice Schreiber <maurice.schreiber@sap.com>
2017-02-01 16:07:03 +0100

    [manila] add quota + limits requests

---
+++ test/requests/shared_file_system/quota_tests.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+describe "Fog::SharedFileSystem::OpenStack | quota requests" do
+  before do
+    @manila = Fog::SharedFileSystem::OpenStack.new
+
+    @project_id = @manila.respond_to?(:current_tenant) ? @manila.current_tenant['id'] : Fog::Mock.random_numbers(6).to_s
+
+    @quota_format = {
+      "gigabytes"          => Integer,
+      "shares"             => Integer,
+      "snapshots"          => Integer,
+      "snapshot_gigabytes" => Integer,
+      "share_networks"     => Integer,
+      "id"                 => String
+    }
+    @quota = @manila.get_quota(@project_id).body['quota_set']
+  end
+
+  describe "success" do
+    it "#get_quota" do
+      @quota.must_match_schema(@quota_format)
+    end
+
+    it "#update_quota" do
+      new_values = @quota.merge(
+        'shares'    => @quota['shares'] + 1,
+        'snapshots' => @quota['snapshots'] + 2
+      )
+
+      @manila.update_quota(@project_id, new_values.clone).status.must_equal 200
+      @manila.get_quota(@project_id).body['quota_set'].must_equal new_values
+      # turn back
+      @manila.update_quota(@project_id, @quota)
+    end
+  end
+end
