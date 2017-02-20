3951c8f7d1f47253a357110d8acbad43b4cbab5b
Darren Hague <d.hague@sap.com>
2017-02-20 08:54:46 +0000

    Revert "Make caching for keystone V3 domains optional and configurable"

--- lib/fog/identity/openstack/v3/models/domains.rb
+++ lib/fog/identity/openstack/v3/models/domains.rb
@@ -9,19 +9,13 @@ module Fog
           model Fog::Identity::OpenStack::V3::Domain
 
           def all(options = {})
-            if service.openstack_cache_ttl > 0
-              cached_domain, expires = Fog::Identity::OpenStack::V3::Domain.cache[{:token   => service.auth_token,
-                                                                                   :options => options}]
-              return cached_domain if cached_domain && expires > Time.now
-            end
-
+            cache = Fog::Identity::OpenStack::V3::Domain.cache
+            cached_domain, expires = cache[{:token => service.auth_token, :options => options}]
+            return cached_domain if cached_domain && expires > Time.now
             domain_to_cache = load_response(service.list_domains(options), 'domains')
-            if service.openstack_cache_ttl > 0
-              cache = Fog::Identity::OpenStack::V3::Domain.cache
-              cache[{:token => service.auth_token, :options => options}] = [domain_to_cache,
-                                                                            Time.now + service.openstack_cache_ttl]
-              Fog::Identity::OpenStack::V3::Domain.cache = cache
-            end
+
+            cache[{:token => service.auth_token, :options => options}] = domain_to_cache, Time.now + 30 # 30-second TTL
+            Fog::Identity::OpenStack::V3::Domain.cache = cache
             domain_to_cache
           end
 
@@ -34,21 +28,15 @@ module Fog
           end
 
           def find_by_id(id)
-            if service.openstack_cache_ttl > 0
-              cached_domain, expires = Fog::Identity::OpenStack::V3::Domain.cache[{:token => service.auth_token,
-                                                                                   :id    => id}]
-              return cached_domain if cached_domain && expires > Time.now
-            end
+            cache = Fog::Identity::OpenStack::V3::Domain.cache
+            cached_domain, expires = cache[{:token => service.auth_token, :id => id}]
+            return cached_domain if cached_domain && expires > Time.now
             domain_hash = service.get_domain(id).body['domain']
             domain_to_cache = Fog::Identity::OpenStack::V3::Domain.new(
               domain_hash.merge(:service => service)
             )
-
-            if service.openstack_cache_ttl > 0
-              cache = Fog::Identity::OpenStack::V3::Domain.cache
-              cache[{:token => service.auth_token, :id => id}] = [domain_to_cache, Time.now + service.openstack_cache_ttl]
-              Fog::Identity::OpenStack::V3::Domain.cache = cache
-            end
+            cache[{:token => service.auth_token, :id => id}] = domain_to_cache, Time.now + 30 # 30-second TTL
+            Fog::Identity::OpenStack::V3::Domain.cache = cache
             domain_to_cache
           end
 
