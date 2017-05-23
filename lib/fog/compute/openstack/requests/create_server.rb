3e2e0f325c5fb1553bf9c3e302f3666b1f44e7ec
Michal Stolarczyk <michal.stolarczyk@workday.com>
2017-05-23 14:17:32 -0700

    Remove some dead code from mocks
    This patch removes two lines of unnecessary code - it looks like the exact same
    calls are done two few lines above.

--- lib/fog/compute/openstack/requests/create_server.rb
+++ lib/fog/compute/openstack/requests/create_server.rb
@@ -169,9 +169,6 @@ module Fog
             response_data['security_groups'] = groups
           end
 
-          data[:last_modified][:servers][server_id] = Time.now
-          data[:servers][server_id] = mock_data
-
           if options['os:scheduler_hints'] && options['os:scheduler_hints']['group']
             group = data[:server_groups][options['os:scheduler_hints']['group']]
             group[:members] << server_id if group
