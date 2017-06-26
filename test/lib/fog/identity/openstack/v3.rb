705283ebdf73a0f2575c135dd4e9b5a830aca3d0
Gilles Dubreuil <gilles@redhat.com>
2017-06-26 15:36:20 +1000

    Removed redundant get_api_version

--- test/lib/fog/identity/openstack/v3.rb
+++ test/lib/fog/identity/openstack/v3.rb
@@ -7,25 +7,6 @@ module Fog
           def initialize(options = {})
           end
         end
-
-        def self.get_api_version(uri, connection_options = {})
-          connection = Fog::Core::Connection.new(uri, false, connection_options)
-          response = connection.request(:expects => [200],
-                                        :headers => {'Content-Type' => 'application/json',
-                                                     'Accept'       => 'application/json'},
-                                        :method  => 'GET')
-
-          body = Fog::JSON.decode(response.body)
-          version = nil
-          unless body['version'].empty?
-            version = body['version']['id']
-          end
-          if version.nil?
-            raise Fog::OpenStack::Errors::ServiceUnavailable, "No version available at #{uri}"
-          end
-
-          version
-        end
       end
     end
   end
