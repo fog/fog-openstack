2cecbcf9fa1f364b6e7167381596b34a40bd19ad
Timur Alperovich <timuralp@swiftstack.com>
2017-07-16 16:58:28 -0700

    [Swift] Fix tests for objects.
    Makes the real tests for Swift object API pass.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,6 +25,42 @@ unless Fog.mocking?
 end
 
 def lorem_file
+  name = File.dirname(__FILE__) + '/lorem.txt'
+  if not File.file?(name)
+    f = File.open(name, 'w')
+    f.write(
+        "Lorem ipsum dolor sit amet, no partem signiferumque vix, feugiat "\
+        "oporteat inciderint an sit, porro oratio concludaturque an vim. Mel "\
+        "commodo eripuit id, ei amet detraxit nec. Duo legendos constituam "\
+        "delicatissimi no, dolor legere ne vix, eros disputationi at per." \
+        "Lucilius perfecto cum an, vel et dicat accommodare, minim reformidans eu" \
+        "sed. Has veri novum dissentiunt id, melius dissentiunt eum id. Quo no" \
+        "vidisse nusquam rationibus, usu ut veri choro. Ex eligendi suscipit nec," \
+        "falli putant vim eu, vix assum graecis reprehendunt ad." \
+        "" \
+        "Has ipsum patrioque evertitur an, vis facete admodum ex, an qui posse" \
+        "erant nihil. Nam sonet salutandi ad, cum oblique disputando et. Ne" \
+        "melius veritus vocibus quo, debet eruditi cu mel. Elitr aperiri atomorum" \
+        "et eum." \
+        "" \
+        "Sea electram prodesset te. Invidunt principes mea in. Natum oblique" \
+        "assueverit ea mea, eos ut solum nullam, an pro simul clita impetus. Per" \
+        "apeirian dissentiunt ne, feugait maluisset reprehendunt cu his. Cu" \
+        "gubergren aliquando moderatius mei, mei saepe impedit ut." \
+        "" \
+        "Te mea facilisis cotidieque definitiones, has illum ullum sensibus id," \
+        "in meliore minimum assentior duo. Ne nec cibo sadipscing mediocritatem," \
+        "lorem fabellas lobortis ut vim, noluisse consetetur temporibus pri ut." \
+        "Qui no nibh appareat delicata, id vis integre debitis, sed probo" \
+        "probatus eu. Vel dicat debet ancillae at, lorem debet ponderum eu cum." \
+        "An sea case affert, option graecis duo ea. Vivendum legendos eum eu." \
+        "" \
+        "Et est inani dolore, dicta prodesset qui ne. Inermis veritus fierent mei" \
+        "no, sit in amet reque philosophia. Erant docendi sit ne. His id" \
+        "petentium periculis. Veri efficiendi no has, id vix quas eripuit" \
+        "temporibus.")
+    f.close
+  end
   File.open(File.dirname(__FILE__) + '/lorem.txt', 'r')
 end
 
