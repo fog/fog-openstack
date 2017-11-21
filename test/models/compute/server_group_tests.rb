c2258c7c230aa613cd56e5b54703e65bd97d1eaa
Mario Manno <mario.manno@gmail.com>
2017-11-21 13:20:44 +0100

    Adapt to houndci code style
    Signed-off-by: Florian Nachtigall <florian.nachtigall@sap.com>

--- test/models/compute/server_group_tests.rb
+++ test/models/compute/server_group_tests.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
 describe "Fog::Compute::OpenStack::ServerGroup" do
-
   describe "validate_server_group_policy" do
-
     it "contains only allowed policies" do
-      ['affinity', 'anti-affinity', 'soft-affinity', 'soft-anti-affinity'].each  do |policy|
+      ['affinity', 'anti-affinity', 'soft-affinity', 'soft-anti-affinity'].each do |policy|
         Fog::Compute::OpenStack::ServerGroup.validate_server_group_policy(policy).must_equal true
       end
     end
