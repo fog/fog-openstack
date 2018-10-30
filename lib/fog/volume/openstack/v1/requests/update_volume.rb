681daee79e9787c9415cd8f4f152c8f7ca779a88
Gilles Dubreuil <gilles@redhat.com>
2018-10-30 15:58:28 +1100

    [backport][v0.3]https://github.com/fog/fog-openstack/pull/433

--- lib/fog/volume/openstack/v1/requests/update_volume.rb
+++ lib/fog/volume/openstack/v1/requests/update_volume.rb
@@ -1,2 +1,19 @@
 require 'fog/volume/openstack/requests/update_volume'
 require 'fog/volume/openstack/v1/requests/real'
+
+module Fog
+  module Volume
+    class OpenStack
+      module Real
+        def update_volume(volume_id, data = {})
+          request(
+            :body    => Fog::JSON.encode('volume' => data),
+            :expects => 200,
+            :method  => 'PUT',
+            :path    => "volumes/#{volume_id}"
+          )
+        end
+      end
+    end
+  end
+end
